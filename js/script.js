6628eec05d2cb99fca88f90ffe2e5f449dda094a
OpenYaji <153737335+OpenYaji@users.noreply.github.com>
2025-02-08 00:25:03 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
-let menuIcon = document.querySelectorAll('#menu-icon');
-let navbar = document.querySelectorAll('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
@@ -24,6 +24,31 @@ window.onscroll = () => {
             });
         };
     });
+
     let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY>100);
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
 };
+
+ScrollReveal({
+    reset: true,
+    distance:'80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container,.portfolio-box,.contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content',{origin:'left'});
+
+const typed = new Typed ('.multiple-text',{
+    strings: ['Frontend Developer','Network Engineer','Data Administrator'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
